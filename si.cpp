06db3ae87b54c2e0270ce0e5da9e8ec4a0249648
Eshant Aggarwal <eshant.7444@outlook.com>
2020-10-01 09:14:50 +0530

    Add files via upload

---
+++ si.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main()
+{float rate, time,principal,si;
+principal=11902646;
+rate=6.75;
+time=3.5;
+si=principal*rate/100*time;
+printf ("\t Hello! ESHANT AGGARWAL here \n\n \t Are you to New to Programming? \n\n\t If Your Answer Is Yes then My Friend this Program is not for you.\n\n\n\t Sorry \t\t\t\t Sorry \t\t\t\t Sorry \n\n");
+printf("\n\t Principal=Rs. %f \n\n \t Rate=%f Percent \n\n\t Time=%f Year\n\n\t Simple Interest=Rs. %f",principal,rate,time,si);
+printf("\n\nHence here you see that you will get Rs. %f of Interest.\n But I assure you that you can't get this.\t Hiiiiii \n\n",si);
+
+
+}
